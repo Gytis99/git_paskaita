d653afe7717bffbd44671ec5008cf5157a7107b7
Gytis9 <privatusomni@gmail.com>
2023-08-18 09:55:35 +0300

    importai, class User

---
+++ model.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Float
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('sqlite:///:memory:')
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    Vardas = Column(String)
+    email = Column(String)
+
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
